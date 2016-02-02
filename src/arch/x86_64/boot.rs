f1526d07d7c3f84de0fab2d76779c7b4e95346e2
Adrian Danis <Adrian.Danis@nicta.com.au>
2016-02-02 16:13:09 +1100

    Implement the fmt::Write trait for debug output

--- src/arch/x86_64/boot.rs
+++ src/arch/x86_64/boot.rs
@@ -1,13 +1,21 @@
 use plat::*;
 use ::config::BootConfig;
+use ::core::fmt::Write;
 
-#[no_mangle]
-pub extern fn boot_system() {
+/* Try and boot the system, potentially returning an error.
+ * Since if we fail there is no way to display or do anything
+ * with the error we do do not bother to have an error type
+ */
+fn try_boot_system() -> Result<(), ()> {
     /* Our current goal is to get this code working
      * with a real config call and init */
     let mut plat = get_platform(&BootConfig);
     plat.init_serial();
-    plat.putchar(b'R');
-    plat.putchar(b'4');
-    plat.putchar(b'\n');
+    try!(write!(plat,"R4\n").or(Err(())));
+    Ok(())
+}
+
+#[no_mangle]
+pub extern fn boot_system() {
+    try_boot_system().unwrap();
 }
--- src/plat/mod.rs
+++ src/plat/mod.rs
@@ -1,10 +1,20 @@
 mod pc99;
 use self::pc99::*;
 use config::BootConfig;
+use ::core::fmt;
 
 pub trait PlatInterface {
     fn init_serial(&mut self);
     fn putchar(&mut self, c: u8);
+}
+
+impl fmt::Write for PlatInterfaceType {
+    fn write_str(&mut self, s: &str) -> ::core::fmt::Result {
+        for byte in s.bytes() {
+            self.putchar(byte);
+        }
+        Ok(())
+    }
 }
 
 /* This wrapper exists to ensure the concrete plat_get_platform
