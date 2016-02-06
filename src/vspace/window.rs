785618eef9595c1e8430143c7fdf00f2f588246d
Adrian Danis <Adrian.Danis@nicta.com.au>
2016-02-07 06:19:22 +1100

    Check the ranges on subwindows

--- src/vspace/window.rs
+++ src/vspace/window.rs
@@ -10,8 +10,20 @@ pub unsafe trait VSpaceWindow<'a> {
      * is a little backwards, but it provides the nicest API as far
      * as I can tell */
     unsafe fn subwindow<'i, I, O: ?Sized>(&self, window: I) -> &'a O where O: VSpaceWindow<'a> + Default, I: VSpaceWindow<'i> {
+        /* Validate the range for this window */
+        if !self.range_valid(window.base(), window.size()) {
+            panic!("Cannot construct window with range {} {}, from {} {}");
+        }
         /* transmate to extend the lifetime. This is safe as
          * it is a zero sized object */
         transmute(&O::default())
+    }
+    fn range_valid(&self, b: usize, s: usize) -> bool {
+        /* We have moved the `- s` to the other side of the equation
+         * so that if `self.base() + self.size()` overflowed
+         * then subtracting `s` will either correct if the range is
+         * valid, or not correct in which case the range is invalid
+         * and the comparison will fail */
+        b >= self.base() && b <= self.base() + self.size() - s
     }
 }
