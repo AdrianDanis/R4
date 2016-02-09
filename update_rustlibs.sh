2fbe07862656899f53d39b0a6e8ac09f87f8178e
Adrian Danis <Adrian.Danis@nicta.com.au>
2016-02-09 14:41:12 +1100

    Support more than just 'libcore' in our custom rebuild crates

--- update_rustlibs.sh
+++ update_rustlibs.sh
@@ -10,11 +10,16 @@ outdir="$rustc/lib/rustlib/$target/lib"
 # Save the location of our patch as an absolute path
 patch=`pwd`/libcore_nofp.patch
 
+if [ "$RUST_GIT_REFERENCE" != "" ]
+then
+    RUST_GIT_REFERENCE="--reference $RUST_GIT_REFERENCE"
+fi
+
 # Move to a temporary directory
 mkdir tmp
 pushd tmp
 # Grab copy of rust repo
-git clone https://github.com/rust-lang/rust.git
+git clone $RUST_GIT_REFERENCE https://github.com/rust-lang/rust.git
 # Apply the disable_float patch
 pushd rust/src
 patch -p0 < $patch
@@ -22,6 +27,9 @@ popd
 # Build libcore and place it where our multirust compiler will find it
 mkdir -p $outdir
 popd
-rustc --target $target --cfg disable_float -Z no-landing-pads --out-dir $outdir tmp/rust/src/libcore/lib.rs
+for lib in libcore librustc_unicode liballoc libcollections
+do
+    rustc --target $target -Z no-landing-pads --cfg disable_float --cfg stdbuild --verbose --out-dir $outdir tmp/rust/src/$lib/lib.rs
+done
 
 rm -rf tmp
