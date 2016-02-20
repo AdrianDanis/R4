ea6b0c9ea95b68c34eeaa9ff4e16e69d11a24232
Adrian Danis <Adrian.Danis@nicta.com.au>
2016-02-20 17:51:09 +1100

    crush some warnings

--- src/arch/x86_64/vspace.rs
+++ src/arch/x86_64/vspace.rs
@@ -2,7 +2,6 @@
 
 use util;
 use ::core::marker::PhantomData;
-use ::core::fmt::Debug;
 use ::core::ops::Deref;
 use vspace::VSpaceWindow;
 use types::*;
@@ -86,7 +85,7 @@ unsafe impl<'a> VSpaceWindow<'a> for BootHighWindow<'a> {
         debug_assert!(self.range_valid(addr, 0));
         HighWindowAddr(addr)
     }
-    unsafe fn new(data: Self::InitData) -> Self {
+    unsafe fn new(_: Self::InitData) -> Self {
         BootHighWindow(PhantomData)
     }
 }
@@ -96,16 +95,16 @@ unsafe impl<'a> VSpaceWindow<'a> for KernelWindow<'a> {
     type InitData = ();
     fn base(&self) -> usize { KERNEL_MAPPING.0 }
     fn size(&self) -> usize { KERNEL_MAPPING.1 }
-    unsafe fn to_paddr(&self, addr: Self::Addr) -> PAddr {
+    unsafe fn to_paddr(&self, _: Self::Addr) -> PAddr {
         unimplemented!()
     }
-    unsafe fn from_paddr(&self, paddr: PAddr) -> Self::Addr {
+    unsafe fn from_paddr(&self, _: PAddr) -> Self::Addr {
         unimplemented!()
     }
-    unsafe fn to_addr(&self, addr: usize) -> Self::Addr {
+    unsafe fn to_addr(&self, _: usize) -> Self::Addr {
         unimplemented!()
     }
-    unsafe fn new(data: Self::InitData) -> Self {
+    unsafe fn new(_: Self::InitData) -> Self {
         KernelWindow(PhantomData)
     }
 }
@@ -126,7 +125,7 @@ unsafe impl<'a> VSpaceWindow<'a> for BootLowWindow<'a> {
         debug_assert!(self.range_valid(addr, 0));
         LowWindowAddr(addr)
     }
-    unsafe fn new(data: Self::InitData) -> Self {
+    unsafe fn new(_: Self::InitData) -> Self {
         BootLowWindow(PhantomData)
     }
 }
